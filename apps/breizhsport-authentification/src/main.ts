aa15f4d0e3efe4b5d9f13f245b2a4a76b626b405
Jessy DROUIN <jessy.drouin@equasens.com>
2024-11-08 09:15:34 +0100

    Modif main.ts breizhsport-front / modif ci.yml for ZAP SCAN

--- apps/breizhsport-authentification/src/main.ts
+++ apps/breizhsport-authentification/src/main.ts
@@ -2,13 +2,15 @@ import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
 const app = express();
+
+// Désactive l'en-tête X-Powered-By pour ne pas divulguer la version d'Express
+app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`Server running at http://${host}:${port}/`);
 });
